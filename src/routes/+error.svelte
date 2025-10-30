51c44f49c437cb2fbaa26e620ad9488dacd3fbd9
ccianos <ccianos@alphavida.com>
2025-10-29 19:05:43 -0700

    Add buttery class to pre tag.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -26,12 +26,16 @@
 </div>
 
 <div class="byterep-container blinking">
-    <pre>{unicodeArt}</pre>
+    <pre class="buttery">{unicodeArt}</pre>
 </div>
 
 <style>
     :root {
         --text-animation-speed: 5s;
+        --color-borders: #7e4000; /* Hazelnut Chocolate (Coffee Shop Palette Color Palette https://www.color-hex.com/color-palette/13328) */
+        --color-buttery-1: #c38e6a; /* Shiracha Brown (Coffee shop Color Palette https://www.color-hex.com/color-palette/3533) */
+        --color-buttery-2:	#b8b17b; /* Leek Powder (Coffee shop Color Palette https://www.color-hex.com/color-palette/3533) */
+        --color-buttery-3: #b8855c; /* Vermicelles (Coffee shop Color Palette https://www.color-hex.com/color-palette/3533) */
     }
 
     div {
@@ -66,6 +70,24 @@
         text-align: left;
         line-height:  0.8;
         font-size: clamp(1px, 1vw, 8px);
+    }
+
+    .buttery {
+        background: linear-gradient(
+            to left,
+            var(--color-buttery-1),
+            var(--color-buttery-2),
+            var(--color-buttery-3),
+            var(--color-buttery-1)
+        );
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        text-align: center;
+        text-decoration: none;
+        text-shadow: 4px 6px 8px var(--color-borders);
+        margin: 0;
+        padding: 0;
     }
 
     .blinking {
